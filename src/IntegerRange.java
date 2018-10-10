29a70726941a4c3d385d3e4d47d8fe910386ee09
benjamin <benjamin.mcbrayer@gmail.com>
2018-10-10 14:22:55 -0400

    October revisions

--- src/IntegerRange.java
+++ src/IntegerRange.java
@@ -7,40 +7,25 @@ public class IntegerRange {
 
 	public static void main(String[] args) {
 
+		// Declare and initialize variables.
 		Scanner scnr = new Scanner(System.in);
-
 		int userNum = 0;
-		String userInfo;
+		String userInfo = null;
 		String playAgain = null;
 
 		// Ask for user information.
 		System.out.print("Please enter your name: ");
-		userInfo = scnr.next();
+		userInfo = scnr.nextLine();
 		System.out.println("Hello, " + userInfo + "!");
 
 		do {// Prompt user to enter an integer.
 			System.out.print("Please enter a number between 1 and 100: ");
 			userNum = scnr.nextInt();
-
-			if (userNum >= 1 && userNum <= 100) {
-				// Subject integer to a series of conditions to determine its range and whether
-				// it is even or odd.
-				System.out.print("Output: " + userNum);
-				if ((userNum / 2 * 2) == userNum) {
-					System.out.print(" is even");
-				}
-				if (((userNum / 2 * 2) == userNum) && ((userNum >= 2) && (userNum <= 25))) {
-					System.out.print(" and less than 25");
-				}
-				if ((userNum / 2 * 2) != userNum) {
-					System.out.print(" is odd");
-				}
-				if (((userNum / 2 * 2) != userNum) && (userNum >= 60)) {
-					System.out.print(" and over 60");
-				}
-			} else {
-				System.out.println(userInfo + ", your number is not between 1 and 100.");
-			}
+			
+			// Subject integer to a series of conditions to determine its range and whether
+			// it is even or odd.
+			automateDecision(userNum, userInfo);
+			
 			// Ask if the user wants to continue entering numbers.
 			System.out.println();
 			System.out.println(userInfo + ", would you like to continue (y/n)?");
@@ -48,10 +33,30 @@ public class IntegerRange {
 			scnr.nextLine();
 
 		} while (playAgain.equalsIgnoreCase("y"));
-		
+
 		// End program officially if user enters anything other than "Y" or "y."
 		System.out.println("Goodbye, " + userInfo + "!");
- 
+
 		scnr.close();
+	}
+
+	public static void automateDecision(int userNum, String userInfo) {
+		if (userNum >= 1 && userNum <= 100) {
+			System.out.print("Output: " + userNum);
+			if ((userNum / 2 * 2) == userNum) {
+				System.out.print(" is even");
+			}
+			if (((userNum / 2 * 2) == userNum) && ((userNum >= 2) && (userNum <= 25))) {
+				System.out.print(" and less than 25");
+			}
+			if ((userNum / 2 * 2) != userNum) {
+				System.out.print(" is odd");
+			}
+			if (((userNum / 2 * 2) != userNum) && (userNum >= 60)) {
+				System.out.print(" and over 60");
+			}
+		} else {
+			System.out.println(userInfo + ", your number is not between 1 and 100.");
+		}
 	}
 }
